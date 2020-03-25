d13a7774eaf30228a0593dbffa2fcfcb640e3271
Francisco Dadone <>
2020-03-25 02:33:02 -0300

    Diseñando la GUI del registro

--- src/main/java/com/franciscodadone/chatty/views/AppFrame.java
+++ src/main/java/com/franciscodadone/chatty/views/AppFrame.java
@@ -20,25 +20,182 @@ public class AppFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        txtUsername = new javax.swing.JLabel();
+        fieldUsername = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
+        txtEmail = new javax.swing.JLabel();
+        jSeparator2 = new javax.swing.JSeparator();
+        fieldPassword = new javax.swing.JPasswordField();
+        fieldEmail = new javax.swing.JTextField();
+        jSeparator3 = new javax.swing.JSeparator();
+        txtAlreadyAcc = new javax.swing.JLabel();
+        SignUpBtn = new javax.swing.JButton();
+        txtPassword1 = new javax.swing.JLabel();
+        txtSignIn = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel1.setAlignmentX(0.0F);
+        jPanel1.setAlignmentY(0.0F);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 526, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jPanel2.setBackground(new java.awt.Color(30, 33, 35));
+        jPanel2.setAlignmentX(0.0F);
+        jPanel2.setAlignmentY(0.0F);
+
+        txtUsername.setBackground(new java.awt.Color(57, 113, 117));
+        txtUsername.setForeground(new java.awt.Color(57, 113, 117));
+        txtUsername.setText("Username");
+
+        fieldUsername.setBackground(new java.awt.Color(32, 33, 35));
+        fieldUsername.setForeground(new java.awt.Color(255, 255, 255));
+        fieldUsername.setBorder(null);
+        fieldUsername.setCaretColor(new java.awt.Color(255, 255, 255));
+
+        txtEmail.setBackground(new java.awt.Color(57, 113, 117));
+        txtEmail.setForeground(new java.awt.Color(57, 113, 117));
+        txtEmail.setText("Email");
+
+        fieldPassword.setBackground(new java.awt.Color(32, 33, 35));
+        fieldPassword.setForeground(new java.awt.Color(255, 255, 255));
+        fieldPassword.setBorder(null);
+        fieldPassword.setCaretColor(new java.awt.Color(255, 255, 255));
+
+        fieldEmail.setBackground(new java.awt.Color(32, 33, 35));
+        fieldEmail.setForeground(new java.awt.Color(255, 255, 255));
+        fieldEmail.setBorder(null);
+        fieldEmail.setCaretColor(new java.awt.Color(255, 255, 255));
+
+        txtAlreadyAcc.setBackground(new java.awt.Color(57, 113, 117));
+        txtAlreadyAcc.setForeground(new java.awt.Color(57, 113, 117));
+        txtAlreadyAcc.setText("Already have an account?");
+
+        SignUpBtn.setBackground(new java.awt.Color(126, 87, 194));
+        SignUpBtn.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        SignUpBtn.setForeground(new java.awt.Color(255, 255, 255));
+        SignUpBtn.setText("Sign Up");
+        SignUpBtn.setBorderPainted(false);
+        SignUpBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SignUpBtnActionPerformed(evt);
+            }
+        });
+
+        txtPassword1.setBackground(new java.awt.Color(57, 113, 117));
+        txtPassword1.setForeground(new java.awt.Color(57, 113, 117));
+        txtPassword1.setText("Password");
+
+        txtSignIn.setBackground(new java.awt.Color(57, 113, 117));
+        txtSignIn.setForeground(new java.awt.Color(57, 113, 117));
+        txtSignIn.setText("Sign in");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(45, 45, 45)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtPassword1)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(fieldPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 314, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(fieldEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 341, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(txtEmail)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtUsername)
+                            .addComponent(jSeparator1)
+                            .addComponent(fieldUsername, javax.swing.GroupLayout.DEFAULT_SIZE, 338, Short.MAX_VALUE))
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addComponent(txtAlreadyAcc)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(txtSignIn))
+                        .addComponent(jSeparator3)
+                        .addComponent(jSeparator2)
+                        .addComponent(SignUpBtn, javax.swing.GroupLayout.DEFAULT_SIZE, 343, Short.MAX_VALUE)))
+                .addContainerGap(46, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(53, 53, 53)
+                .addComponent(txtUsername)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(fieldUsername, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(24, 24, 24)
+                .addComponent(txtEmail)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(fieldEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(txtPassword1)
+                .addGap(18, 18, 18)
+                .addComponent(fieldPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 12, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(36, 36, 36)
+                .addComponent(SignUpBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtAlreadyAcc)
+                    .addComponent(txtSignIn))
+                .addContainerGap(99, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 922, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 489, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void SignUpBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SignUpBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_SignUpBtnActionPerformed
+
    
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton SignUpBtn;
+    private javax.swing.JTextField fieldEmail;
+    private javax.swing.JPasswordField fieldPassword;
+    private javax.swing.JTextField fieldUsername;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JLabel txtAlreadyAcc;
+    private javax.swing.JLabel txtEmail;
+    private javax.swing.JLabel txtPassword1;
+    private javax.swing.JLabel txtSignIn;
+    private javax.swing.JLabel txtUsername;
     // End of variables declaration//GEN-END:variables
 }
